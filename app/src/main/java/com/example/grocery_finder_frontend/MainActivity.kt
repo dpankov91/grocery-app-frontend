53360b5ce5a108ef69e35eb6bf6331f6ccf605cf
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2021-04-25 21:09:27 +0200

    Changes and corrections, plus fake data

--- app/src/main/java/com/example/grocery_finder_frontend/MainActivity.kt
+++ app/src/main/java/com/example/grocery_finder_frontend/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,14 +13,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupDataObserver()
+        val shops = arrayOf("Bilka", "Fakta", "Rema 1000", "Lidl", "Netto")
+
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(
+                this, android.R.layout.simple_list_item_1, shops
+        )
+        shopList.adapter = arrayAdapter
+        shopList.onItemClickListener = AdapterView.OnItemClickListener{_, _, pos, _ -> onListItemClick(pos)}
     }
 
-    private fun setupDataObserver() {
-
+    private fun onListItemClick(pos: Int) {
+        Toast.makeText(this, "You have clicked this one at position $pos", Toast.LENGTH_LONG).show()
     }
 
-    fun OnCreateOptionsMenu(menu: Menu?): Boolean{
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean{
         menuInflater.inflate(R.menu.menu1, menu)
 
         return true
