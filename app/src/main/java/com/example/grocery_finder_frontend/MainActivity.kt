a8fa7e24728546573eae176b23ecb7aec308353f
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2021-04-25 16:40:22 +0200

    Working welcome

--- app/src/main/java/com/example/grocery_finder_frontend/MainActivity.kt
+++ app/src/main/java/com/example/grocery_finder_frontend/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.grocery_finder_frontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupDataObserver()
     }
+
+    private fun setupDataObserver() {
+
+    }
+
+    fun OnCreateOptionsMenu(menu: Menu?): Boolean{
+        menuInflater.inflate(R.menu.menu1, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.img_logo -> Toast.makeText(this, "Show MapActivity", Toast.LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
---
+++ app/src/main/java/com/example/grocery_finder_frontend/WelcomeActivity.kt
@@ -0,0 +1,21 @@
+package com.example.grocery_finder_frontend
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        img_logo.alpha = 0f
+        img_logo.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
