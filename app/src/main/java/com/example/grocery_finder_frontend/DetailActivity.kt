8cee882a516d09b4773d43c0a4666745daf93f9a
dpankov91 <dpankov91@gmail.com>
2021-04-26 17:22:27 +0300

    get current location and set it as extra into intent

--- app/src/main/java/com/example/grocery_finder_frontend/DetailActivity.kt
+++ app/src/main/java/com/example/grocery_finder_frontend/DetailActivity.kt
@@ -1,18 +1,26 @@
 package com.example.grocery_finder_frontend
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
     private val LOG = "abc"
+    private var lat = 0.0
+    private var long = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +31,7 @@ class DetailActivity : AppCompatActivity() {
         finish()
     }
 
-    fun onClickGoToMap(view: View) {
-
-    }
+    // fun onClickGoToMap(view: View) {}
 
     fun onClickOpenWebsite(view: View) {
         val url = "http://www.netto.dk"
@@ -33,5 +39,48 @@ class DetailActivity : AppCompatActivity() {
         val browserIntent = Intent(Intent.ACTION_VIEW)
         browserIntent.data = Uri.parse(url)
         startActivity(browserIntent)
+    }
+
+    private val permissions = arrayOf(
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+    @SuppressLint("MissingPermission")
+    fun onClickGoToMap(view: View) {
+        requestPermissions()
+        if(!isPermissionGiven()){
+            Toast.makeText(this, "Permission to use GPS is denied", Toast.LENGTH_LONG).show()
+            return
+        }
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        if(location != null){
+            tvCoordinates.text = "${location.latitude}, ${location.longitude}"
+            lat = location.latitude
+            long = location.longitude
+            Log.d(LOG, lat.toString() + " " + long.toString())
+            /* val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra("lat",lat)
+            intent.putExtra("long", long)
+            startActivity(intent) */
+        } else{
+            Toast.makeText(this, "The Location is unfortunately null", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isPermissionGiven(): Boolean{
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            return permissions.all {f -> checkSelfPermission(f) == PackageManager.PERMISSION_GRANTED}
+        }
+        return true
+    }
+    private fun requestPermissions(){
+        if(!isPermissionGiven()){
+            Log.d("TAG", "Permission denied to use GPS - requesting it")
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(permissions, 1)
+            } else
+                Log.d("TAG", "Permission to use GPS Granted")
+        }
     }
 }
