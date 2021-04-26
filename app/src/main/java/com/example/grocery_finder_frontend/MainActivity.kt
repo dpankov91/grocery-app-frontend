7cb5593014f17c5d3158cb984cc18914c4695c82
ArmNem <58069062+ArmNem@users.noreply.github.com>
2021-04-26 14:48:04 +0200

    lo

--- app/src/main/java/com/example/grocery_finder_frontend/MainActivity.kt
+++ app/src/main/java/com/example/grocery_finder_frontend/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.grocery_finder_frontend
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       gotomap()
     }
+    private fun gotomap(){
+        var intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
